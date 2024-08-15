9a6855f668ef09a12bb8c8641bf3c6ac7402a501
Debanjan <debanjanghosal10@gmail.com>
2024-08-15 09:44:39 +0530

    Updated Footer Responsive Design

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -4,16 +4,16 @@ import Link from 'next/link';
 const Footer = () => {
     return (
         <div className="container ">
-            <footer className="justify-center row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
-                <div className="col mb-3">
+            <footer className="flex justify-center row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+                <div className="flex justify-center col mb-3">
                     <h1 className='text-2xl font-extrabold'>🚗  CarHub</h1>
-                    <p className="text-body-secondary">CarHub {new Date().getFullYear()}</p>
-                    <p className="text-body-secondary">All Rights Reserved ©</p>
+                    {/* <p className="text-body-secondary">CarHub {new Date().getFullYear()}</p>
+                    <p className="text-body-secondary">All Rights Reserved ©</p> */}
                 </div>
 
-                <div className="col mb-3">
+                <div className="flex items-center flex-col col mb-3">
                     <h5>About</h5>
-                    <ul className="nav flex-column">
+                    <ul className="nav flex-column text-center">
                         <li className="nav-item mb-2"><Link href="#" className="nav-link p-0 text-body-secondary">How it Works</Link></li>
                         <li className="nav-item mb-2"><Link href="#" className="nav-link p-0 text-body-secondary">Featured</Link></li>
                         <li className="nav-item mb-2"><Link href="#" className="nav-link p-0 text-body-secondary">Partnership</Link></li>
@@ -22,9 +22,9 @@ const Footer = () => {
                     </ul>
                 </div>
 
-                <div className="col mb-3">
+                <div className="flex items-center flex-col col mb-3">
                     <h5>Company</h5>
-                    <ul className="nav flex-column">
+                    <ul className="nav flex-column text-center">
                         <li className="nav-item mb-2"><Link href="#" className="nav-link p-0 text-body-secondary">Events</Link></li>
                         <li className="nav-item mb-2"><Link href="#" className="nav-link p-0 text-body-secondary">Blog</Link></li>
                         <li className="nav-item mb-2"><Link href="#" className="nav-link p-0 text-body-secondary">Podcast</Link></li>
@@ -33,9 +33,9 @@ const Footer = () => {
                     </ul>
                 </div>
 
-                <div className="col mb-3">
+                <div className="flex items-center flex-col col mb-3">
                     <h5>Socials</h5>
-                    <ul className="nav flex-column">
+                    <ul className="nav flex-column text-center">
                         <li className="nav-item mb-2"><Link href="#" className="nav-link p-0 text-body-secondary">Discord</Link></li>
                         <li className="nav-item mb-2"><Link href="#" className="nav-link p-0 text-body-secondary">Instagram</Link></li>
                         <li className="nav-item mb-2"><Link href="#" className="nav-link p-0 text-body-secondary">Twitter</Link></li>
@@ -45,7 +45,8 @@ const Footer = () => {
                 </div>
 
             </footer >
-            <div className='mx-auto flex flex-wrap justify-between '>
+
+            <div className='mx-auto flex flex-wrap justify-center gap-4'>
                 <p className='text-slate-600'>©2024 CarHub. All rights reserved</p>
                 <div className='flex gap-4'>
                     <Link href='/' className='no-underline text-slate-600'>Privacy Policy</Link>
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
-import Button from './Button'
 import Link from 'next/link'
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 
 
 const Navbar = () => {
     return (
         <header className='flex justify-between px-6 md:px-16 py-4'>
             <Link href="/" className='no-underline text-black font-extrabold text-xl md:text-2xl'>  🚗  CarHub</Link>
-            <Link href='/' className='no-underline  p-2 rounded-full'>Sign in</Link>
+            <TabGroup>
+                <TabList>
+                    <Link href='/checkout' className='no-underline  p-2 rounded-full'>
+                        <Tab className='data-[selected]:bg-blue-500 rounded-md p-2 border-none data-[selected]:text-white text-base md:text-xl mx-2  md:w-24 lg:w-32'>
+                            Checkout
+                        </Tab>
+                    </Link>
+
+                    <Link href='/pricing' className='no-underline  p-2 rounded-full'>
+                        <Tab className='data-[selected]:bg-blue-500 rounded-md p-2 border-none data-[selected]:text-white text-base md:text-xl mx-2  md:w-24 lg:w-32'>
+                            Pricing
+                        </Tab>
+                    </Link>
+
+                    <Link href='/faqs' className='no-underline  p-2 rounded-full'>
+                        <Tab className='data-[selected]:bg-blue-500 rounded-md p-2 border-none data-[selected]:text-white text-base md:text-xl mx-2  md:w-24 lg:w-32'>
+                            FAQs
+                        </Tab>
+                    </Link>
+
+                </TabList>
+            </TabGroup>
+
         </header >
     )
 }
