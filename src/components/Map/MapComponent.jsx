79574f03edb8fe953e0abd89c116626a190f0a1a
Debanjan <debanjanghosal10@gmail.com>
2024-08-26 17:56:12 +0530

    Added Detect My Location functionality

--- src/components/Map/MapComponent.jsx
+++ src/components/Map/MapComponent.jsx
@@ -1,6 +1,6 @@
 'use client'
-import React, { useState } from 'react'
-import { MapContainer, TileLayer, Marker, Popup, useMapEvent } from 'react-leaflet'
+import React, { useEffect, useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvent, useMap } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import { Icon } from 'leaflet';
 import axios from 'axios';
@@ -13,6 +13,18 @@ const MapComponent = () => {
     const [loading, setLoading] = useState(false)
     const [address, setAddress] = useState({ city: "", suburb: "", neighbourhood: "", postcode: "", state: "" })
 
+
+    const UpdateMapCenter = () => {
+        const map = useMap()
+
+        useEffect(() => {
+            map.setView([geocode.lat, geocode.lng])
+        }, [map, geocode])
+
+    }
+
+
+
     const MapEventComponent = () => {
         useMapEvent('click', (event) => {
             const { lat, lng } = event.latlng;
@@ -20,6 +32,7 @@ const MapComponent = () => {
         })
 
     }
+
 
     const customMarkerIcon = new Icon({
         iconUrl: "https://img.icons8.com/?size=100&id=13800&format=png&color=000000",
@@ -34,24 +47,58 @@ const MapComponent = () => {
 
             console.log(response);
 
-            const { city, town, postcode, suburb, state } = response.data.response.address;
+            let { city, town, postcode, suburb, state } = response.data.response.address;
 
-            if (!city) {
-                setAddress({ city: town, postcode, suburb, state })
-            } else {
-                setAddress({ city, postcode, suburb, state })
-            }
-
-
-
+            // Set fallback values to avoid undefined
+            city = city || town || "Unknown City";
+            suburb = suburb || "Unknown Suburb";
+            state = state || "Unknown State";
+            postcode = postcode || "000000";
+            setAddress({ city, postcode, suburb, state })
 
         } catch (error) {
             console.log("ERROR - ", error);
-
         }
+
         finally {
             setLoading(false)
         }
+    }
+
+    const getUserLocation = () => {
+
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            try {
+                setLoading(true)
+                // getting user coordinates from geolocation api (in-built in browsers)
+                const { latitude, longitude } = position.coords;
+                console.log(latitude, longitude);
+
+                //saving user coordinates as state
+                setGeocode({ lat: latitude, lng: longitude })
+                // making API request to get address from coordinates
+                const response = await axios.get(`http://localhost:3000/api/get-location?lat=${geocode.lat}&lng=${geocode.lng}`)
+
+                let { city, town, postcode, suburb, state } = response.data.response.address;
+
+                // Set fallback values to avoid undefined
+                city = city || town || "Unknown City";
+                suburb = suburb || "Unknown Suburb";
+                state = state || "Unknown State";
+                postcode = postcode || "000000";
+
+                //setting the state address
+                setAddress({ city, postcode, suburb, state })
+
+
+            } catch (error) {
+                console.log(error);
+            }
+            finally {
+                setLoading(false)
+            }
+        })
+
     }
 
 
@@ -67,9 +114,11 @@ const MapComponent = () => {
 
             </div>
 
-            <MapContainer center={[22.5744, 88.3629]} zoom={13} className='z-10 h-[45vh] w-[45vw]'>
+            <MapContainer center={[geocode.lat, geocode.lng]} zoom={13} className='z-10 h-[45vh] w-[45vw]'>
                 <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url='https://tile.openstreetmap.org/{z}/{x}/{y}.png' />
+
+                <UpdateMapCenter />
 
                 <Marker position={geocode} icon={customMarkerIcon}>
                     <Popup>
@@ -81,8 +130,10 @@ const MapComponent = () => {
 
             </MapContainer>
 
-            <div className='flex justify-center'>
+            <div className='flex justify-center gap-4'>
                 <button type='btn' onClick={getAddress} className='bg-slate-900 p-2 w-fit rounded-md text-white mt-2'>Get Address</button>
+
+                <button type='btn' onClick={getUserLocation} className='text-slate-900 p-2 w-fit rounded-md bg-white mt-2'>Detect My Location</button>
             </div>
         </div>
     )
