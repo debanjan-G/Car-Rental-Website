062f3c6d33b9bb264163228d4cc8c0413db2aedc
Debanjan <debanjanghosal10@gmail.com>
2024-08-27 10:44:02 +0530

    Created API to get A car by name

---
+++ src/app/api/cars/[carName]/route.js
@@ -0,0 +1,24 @@
+import Cars from "@/models/CarModel";
+import { NextResponse } from "next/server";
+
+export async function GET(req, { params }) {
+  try {
+    const { carName } = params;
+    console.log("CAR NAME = ", carName);
+
+    const car = await Cars.findOne({ name: carName });
+    console.log("CAR DOCUMENT = ", car);
+
+    if (!car) {
+      return NextResponse.json({ success: false, msg: "Car Not Found!" });
+    }
+
+    return NextResponse.json({ success: true, car });
+  } catch (error) {
+    console.log("ERROR -> ", error);
+    return NextResponse.json(
+      { success: false, msg: "Something Went Wrong. Check server console" },
+      { status: 500 }
+    );
+  }
+}
