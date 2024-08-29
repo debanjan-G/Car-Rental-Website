38674fb9b1f4d5b1a10d2bd8343a4fd73fb31e28
Debanjan <debanjanghosal10@gmail.com>
2024-08-29 11:03:15 +0530

    Test Commit 01

--- src/app/api/cars/[carName]/route.js
+++ src/app/api/cars/[carName]/route.js
@@ -6,14 +6,15 @@ export async function GET(req, { params }) {
     const { carName } = params;
     console.log("CAR NAME = ", carName);
 
-    const car = await Cars.findOne({ name: carName }).hint({ name: 1 });
-    console.log("CAR DOCUMENT = ", car);
+    // const car = await Cars.findOne({ name: carName }).hint({ name: 1 });
+    // console.log("CAR DOCUMENT = ", car);
 
-    if (!car) {
-      return NextResponse.json({ success: false, msg: "Car Not Found!" });
-    }
+    // if (!car) {
+    //   return NextResponse.json({ success: false, msg: "Car Not Found!" });
+    // }
 
-    return NextResponse.json({ success: true, car });
+    return NextResponse.json({ success: true, msg: "Hey there!" });
+    // return NextResponse.json({ success: true, car });
   } catch (error) {
     console.log("ERROR -> ", error);
     return NextResponse.json(
